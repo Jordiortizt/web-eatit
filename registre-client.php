00cca72fc7505749b2068b189d1f1370872ab61a
Jordi <jordiortizt@gmail.com>
2021-05-20 12:58:45 +0200

    registre client alpha2

--- registre-client.php
+++ registre-client.php
@@ -61,33 +61,30 @@
                     dni
                     direccion
                     ciudad
-                    <form action="" onsubmit="return false;">
-                      <div class="row">
-                        <div class="mb-3 col-6">
-                          <label for="nom" class="form-label">Nom</label>
-                          <input type="text" class="form-control" id="usuari">
-                          <div id="errorNom" class="form-text text-danger"></div>
-                        </div>
 
-                        <div class="mb-3 col-6">
-                          <label for="cognom" class="form-label">Cognoms</label>
-                          <input type="text" class="form-control" id="usuari">
-                          <div id="errorCognom" class="form-text text-danger"></div>
-                        </div>
+                    <form action="" onsubmit="return false;">
+                      <div class="mb-3">
+                        <label for="nom" class="form-label">Nom</label>
+                        <input type="text" class="form-control" id="usuari">
+                        <div id="errorNom" class="form-text text-danger"></div>
+                      </div>
+
+                      <div class="mb-3">
+                        <label for="cognom" class="form-label">Cognoms</label>
+                        <input type="text" class="form-control" id="usuari">
+                        <div id="errorCognom" class="form-text text-danger"></div>
                       </div>
                       
-                      <div class="row">
-                        <div class="mb-3 col-6">
-                          <label for="nom" class="form-label">Telefon</label>
-                          <input type="text" class="form-control" id="usuari">
-                          <div id="errorNom" class="form-text text-danger"></div>
-                        </div>
+                      <div class="mb-3">
+                        <label for="tel" class="form-label">Telefon</label>
+                        <input type="number" class="form-control" id="tel">
+                        <div id="errorTel" class="form-text text-danger"></div>
+                      </div>
 
-                        <div class="mb-3 col-6">
-                          <label for="cognom" class="form-label">DNI</label>
-                          <input type="text" class="form-control" id="usuari">
-                          <div id="errorCognom" class="form-text text-danger"></div>
-                        </div>
+                      <div class="mb-3">
+                        <label for="dni" class="form-label">DNI</label>
+                        <input type="text" class="form-control" id="dni">
+                        <div id="errorDni" class="form-text text-danger"></div>
                       </div>
 
                       <div class="mb-3">
@@ -97,21 +94,9 @@
                       </div>
 
                       <div class="mb-3">
-                        <label for="usuari" class="form-label">Correu Electronic</label>
-                        <input type="text" class="form-control" id="usuari">
-                        <div id="errorUsuari" class="form-text text-danger"></div>
-                      </div>
-
-                      <div class="mb-3">
-                        <label for="usuari" class="form-label">Telefon</label>
-                        <input type="text" class="form-control" id="usuari">
-                        <div id="errorUsuari" class="form-text text-danger"></div>
-                      </div>
-
-                      <div class="mb-3">
-                        <label for="usuari" class="form-label">Usuari o Correu Electronic</label>
-                        <input type="text" class="form-control" id="usuari">
-                        <div id="errorUsuari" class="form-text text-danger"></div>
+                        <label for="email" class="form-label">Correu Electronic</label>
+                        <input type="email" class="form-control" id="email">
+                        <div id="errorEmail" class="form-text text-danger"></div>
                       </div>
                       
                       <div class="mb-3">
