95f61fa9e39ad45304be00e3aee98dd42c6b4201
Jordi <jordiortizt@gmail.com>
2021-05-20 09:30:16 +0200

    Update login.php

--- php/login.php
+++ php/login.php
@@ -1,10 +1,12 @@
 <?php
     require_once("./functions.php");
 
-    $usuari = $_POST["usuari"];
-    $password = $_POST["password"];
+    // $usuari = $_POST["usuari"];
+    // $password = $_POST["password"];
+    $usuari = "Andreu@gmail.com"
+    $password = "P@ssw0rd";
     // $password = hash('sha512',$password);
-    $params = "?usuario=" . $usuari . "&correo=" . $usuari . "&password=" . $password;
+    $params = "?Usuario=" . $usuari . "&Email=" . $usuari . "&Password=" . $password;
     $peticio = peticionGet("usuarios", $params)->usuarios;
     if(count($peticio) == 1){
         session_start();
