1dd57df7acb695b26e6dca692ae52f3f09903d55
Jordi <jordiortizt@gmail.com>
2021-05-20 09:40:37 +0200

    Update login.php

--- php/login.php
+++ php/login.php
@@ -8,11 +8,11 @@
     // $password = hash('sha512',$password);
     $params = "?Usuario=" . $usuari . "&Email=" . $usuari . "&Password=" . $password;
     $peticio = peticionGet("usuarios", $params)->usuarios;
-    if(count($peticio) == 1){
-        session_start();
-        $_SESSION["usuariEatit"] = $peticio[0];
-        echo 1;
-    }else{
-        echo 0;
-    }
+    // if(count($peticio) == 1){
+    //     session_start();
+    //     $_SESSION["usuariEatit"] = $peticio[0];
+    //     echo 1;
+    // }else{
+    //     echo 0;
+    // }
 ?>
