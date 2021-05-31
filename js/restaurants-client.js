9ded5a01973d5e5c2357d95099cd749a6af44c96
jayrule360 <jayrule360@gmail.com>
2021-05-31 12:30:38 +0200

    Movidas

--- js/restaurants-client.js
+++ js/restaurants-client.js
@@ -14,3 +14,19 @@ window.onload = function restaurantsClient() {
     xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhttp.send("");
 };
+function escollirRestaurant(id) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            if (this.response === "1") {
+                window.location.href = './productes-client.php';
+            }
+            else {
+                document.getElementById("mostrarRestaurants").innerHTML = this.response;
+            }
+        }
+    };
+    xhttp.open("POST", "./php/productes-restaurant.php", true);
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhttp.send("idRestaurant=" + id);
+}
