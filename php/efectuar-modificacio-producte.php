5821855e7d07ad78f6d6dde47d1b03ef2c5725fc
jayrule360 <jayrule360@gmail.com>
2021-06-03 18:31:45 +0200

    Fix

--- php/efectuar-modificacio-producte.php
+++ php/efectuar-modificacio-producte.php
@@ -7,6 +7,8 @@
     $params = "?Nombre=" . $tel."&id".intval($restaurant->ID);
     $peticion = peticionGet('platos',$params)->platos;
 
+    $img = $_POST["img"];
+
     $total = count($peticion);
 
     if($total > 0){
@@ -14,10 +16,21 @@
         return 1;
     }
 
-    $fotoName = $_FILES["foto"]["name"];
-    $fotoTmp = $_FILES["foto"]["tmp_name"];
+//    $fotoName = $_FILES["foto"]["name"];
+//    $fotoTmp = $_FILES["foto"]["tmp_name"];
+//
+//    $fotoName = str_replace(" ","",$fotoName);
 
-    $fotoName = str_replace(" ","",$fotoName);
+    $params = $_POST["id"];
+    $peticion = peticionGet('platos',$params)->platos;
+
+    if($peticion[0]->URLFoto == $fotoName){
+        $arrayParams["Plato"] = $_POST["nom"];
+        $arrayParams["Precio"] = floatval($_POST["preu"]);
+        $arrayParams["Descripcion"] = $_POST["desc"];
+
+        $peticio = peticionPut("platos/".intval($_POST["id"]),$arrayParams);
+    }else{
 try{
     $result = $s3Client->putObject([
         'Bucket' => $bucket,
@@ -29,13 +42,32 @@ try{
     echo $fotoName;
     print_r($error);
 }
-
     $arrayParams["Plato"] = $_POST["nom"];
     $arrayParams["Precio"] = floatval($_POST["preu"]);
     $arrayParams["Descripcion"] = $_POST["desc"];
     $arrayParams["URLFoto"] = $s3_route;
 
     $peticio = peticionPut("platos/".intval($_POST["id"]),$arrayParams);
+    }
+
+//try{
+//    $result = $s3Client->putObject([
+//        'Bucket' => $bucket,
+//        'Key' => 'Platos/' . $fotoName,
+//        'SourceFile' => $fotoTmp,
+//    ]);
+//    $s3_route = "https://s3ortizjairo.s3-eu-west-3.amazonaws.com/Platos/" . $fotoName;
+//}catch(Exception $error){
+//    echo $fotoName;
+//    print_r($error);
+//}
+
+//    $arrayParams["Plato"] = $_POST["nom"];
+//    $arrayParams["Precio"] = floatval($_POST["preu"]);
+//    $arrayParams["Descripcion"] = $_POST["desc"];
+////    $arrayParams["URLFoto"] = $s3_route;
+//
+//    $peticio = peticionPut("platos/".intval($_POST["id"]),$arrayParams);
     
     header("Location: ../productes-propietari.php");
 ?>
